a7da1db08b42c9e01574f7eadc3f1e1413f0ed50
Stefan Eiermann <foss@ultraapp.de>
2021-07-25 21:36:45 +0200

    fix entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 
 cp -r /statichtml /usr/share/nginx/html
@@ -6,4 +6,3 @@ cp -r /statichtml /usr/share/nginx/html
 find /usr/share/nginx/html -type f -exec sed -i "s/%%API_ENDPOINT_URL%%/$API_ENDPOINT_URL/g" {} \;
 
 
-/docker-entrypoint.sh $@
