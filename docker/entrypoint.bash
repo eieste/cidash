606010c7796d739222232a66d34c2afdd1049cbf
Stefan Eiermann <foss@ultraapp.de>
2021-07-25 19:47:50 +0200

    Try to fix docker build

--- docker/entrypoint.bash
+++ docker/entrypoint.bash
@@ -6,4 +6,4 @@ cp -r /statichtml /usr/share/nginx/html
 find /usr/share/nginx/html -type f -exec sed -i "s/%%API_ENDPOINT_URL%%/$API_ENDPOINT_URL/g" {} \;
 
 
-/docker-entrypoint.sh
+/docker-entrypoint.sh $@
